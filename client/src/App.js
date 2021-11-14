8d0b281981eae9eae5f951ee6def2bbd3095d059
amine.kaabachi <amine.kaabachi@gmail.com>
2021-11-14 13:39:19 +0100

    refacto smart contract

--- client/src/App.js
+++ client/src/App.js
@@ -1,83 +1,151 @@
 import React, { Component } from "react";
 import ChangeMeContract from "./contracts/ChangeMe.json";
 import getWeb3 from "./getWeb3";
-import { LinkPreview } from '@dhaiwat10/react-link-preview';
+import { LinkPreview } from "@dhaiwat10/react-link-preview";
+import FilledInput from "@mui/material/FilledInput";
+import Button from "@mui/material/Button";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import Stack from "@mui/material/Stack";
+import Chip from "@mui/material/Chip";
+import * as Icon from "react-cryptocoins";
 
 import "./App.css";
 
 class App extends Component {
-    state = { web3: null, accounts: null, contract: null, url: '', price: 0, newUrl: '', newPrice: 0 };
+  state = {
+    web3: null,
+    accounts: null,
+    contract: null,
+    url: "",
+    price: 0,
+    owner: null,
+    gasprice: 0,
+    newUrl: "",
+    newPrice: 0,
+  };
 
-    componentDidMount = async() => {
-        try {
-            // Get network provider and web3 instance.
-            const web3 = await getWeb3();
+  componentDidMount = async () => {
+    try {
+      const web3 = await getWeb3();
+      const accounts = await web3.eth.getAccounts();
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = ChangeMeContract.networks[networkId];
+      const instance = new web3.eth.Contract(
+        ChangeMeContract.abi,
+        deployedNetwork && deployedNetwork.address
+      );
 
-            // Use web3 to get the user's accounts.
-            const accounts = await web3.eth.getAccounts();
-
-            // Get the contract instance.
-            const networkId = await web3.eth.net.getId();
-            const deployedNetwork = ChangeMeContract.networks[networkId];
-            const instance = new web3.eth.Contract(
-                ChangeMeContract.abi,
-                deployedNetwork && deployedNetwork.address,
-            );
-
-            // Set web3, accounts, and contract to the state, and then proceed with an
-            // example of interacting with the contract's methods.
-            this.setState({ web3, accounts, contract: instance }, this.init);
-        } catch (error) {
-            // Catch any errors for any of the above operations.
-            alert(
-                `Failed to load web3, accounts, or contract. Check console for details.`,
-            );
-            console.error(error);
-        }
-    };
-
-    init = async() => {
-        const { contract } = this.state;
-        const { link, amount } = await contract.methods.get().call();
-        this.setState({ url: link, price: amount });
-    };
-
-    _handleUrlChange = (event) => {
-        const { value } = event.target
-
-        this.setState({
-            newUrl: value
-        })
+      this.setState({ web3, accounts, contract: instance }, this.init);
+    } catch (error) {
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`
+      );
+      console.error(error);
     }
+  };
 
-    _handlePriceChange = (event) => {
-      const { value } = event.target
+  init = async () => {
+    const { web3, contract } = this.state;
+    const url = await contract.methods.getURL().call();
+    const price = await contract.methods.getPrice().call();
+    const owner = await contract.methods.getOwner().call();
+    const gasprice = await contract.methods.getGasPrice().call();
 
-      this.setState({
-          newPrice: value
-      })
+    this.setState({
+      url,
+      price: price ? web3.utils.fromWei(price, "ether") : 0,
+      owner,
+      gasprice: gasprice ? web3.utils.fromWei(gasprice, "ether") : 0,
+    });
+  };
+
+  _handleUrlChange = (event) => {
+    const { value } = event.target;
+
+    this.setState({
+      newUrl: value,
+    });
+  };
+
+  _handlePriceChange = (event) => {
+    const { value } = event.target;
+
+    this.setState({
+      newPrice: value,
+    });
+  };
+
+  _changeUrl = async () => {
+    const { web3, accounts, contract, newUrl, newPrice } = this.state;
+    await contract.methods
+      .set(newUrl)
+      .send({ from: accounts[0], value: web3.utils.toWei(newPrice.toString(), "ether") });
+    await this.init();
+  };
+
+  render() {
+    if (!this.state.web3) {
+      return <div> Loading Web3, accounts, and contract... </div>;
     }
-
-    _changeUrl = async() => {
-      const { accounts, contract, newUrl, newPrice } = this.state;
-      await contract.methods.set(newUrl, newPrice).send({ from: accounts[0] });
-      await this.init()
-    }
-
-    render() {
-        if (!this.state.web3) {
-            return <div> Loading Web3, accounts, and contract... </div>;
-        }
-        return ( <div className="App">
-                <div className="change">
-                  <input placeholder={this.state.url} type="text" value={ this.state.newUrl } onChange={this._handleUrlChange} />
-                  <input placeholder={this.state.price} type="text" value={ this.state.newPrice } onChange={this._handlePriceChange} />
-                  <button type="submit" onClick={ this._changeUrl }>Change Me</button>
-                </div>
-                <LinkPreview url={ this.state.url }  width='400px' />
-            </div>
-        );
-    }
+    return (
+      <div className="App">
+        <div className="change">
+          <FormControl
+            size="small"
+            variant="filled"
+            sx={{ m: 1, width: "25ch" }}
+          >
+            <InputLabel>URL</InputLabel>
+            <FilledInput
+              placeholder={this.state.url}
+              value={this.state.newUrl}
+              onChange={this._handleUrlChange}
+            />
+          </FormControl>
+          <FormControl
+            size="small"
+            variant="filled"
+            sx={{ m: 1, width: "15ch" }}
+          >
+            <InputLabel>PRICE</InputLabel>
+            <FilledInput
+              endAdornment={
+                <InputAdornment position="start">
+                  <Icon.EthAlt size={18} />
+                </InputAdornment>
+              }
+              placeholder={this.state.price}
+              value={this.state.newPrice}
+              onChange={this._handlePriceChange}
+            />
+          </FormControl>
+          <Button
+            size="large"
+            variant="contained"
+            onClick={this._changeUrl}
+            sx={{ m: 1, mt: 1.5 }}
+            color="success"
+          >
+            Update
+          </Button>
+        </div>
+        <LinkPreview url={this.state.url} width="400px" />
+        <Stack direction="row" spacing={1} sx={{ m: 2 }}>
+          <Chip
+            label={"PRICE: " + this.state.price + ""}
+            icon={<Icon.EthAlt size={18} />}
+          />
+          <Chip label={"OWNER: " + this.state.owner} />
+          <Chip
+            label={"GAS: " + this.state.gasprice + ""}
+            icon={<Icon.EthAlt size={18} />}
+          />
+        </Stack>
+      </div>
+    );
+  }
 }
 
 export default App;
--- contracts/ChangeMe.sol
+++ contracts/ChangeMe.sol
@@ -1,19 +1,50 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.21 <0.7.0;
+pragma experimental ABIEncoderV2;
 
 contract ChangeMe {
-  string public url = "https://ethereum.org/en/";
-  uint public price = 0;
+    address payable contractOwner;
 
-  function set(string memory x, uint amount) public {
-    if (amount > price) {
-      url = x;
-      price = amount;
+    struct Update {
+        string url;
+        address owner;
+        uint256 price;
+        uint256 gasprice;
     }
-  }
 
-  function get() public view returns (string memory link, uint amount) {
-    return (url, price);
-  }
-  
+    Update public state;
+
+    event UpdateURL(string url, uint256 price, address owner);
+
+    constructor() public {
+        state = Update("https://ethereum.org/en/", tx.origin, 0, tx.gasprice);
+        emit UpdateURL(state.url, state.price, state.owner);
+        contractOwner = tx.origin;
+    }
+
+    function set(string memory newURL) public payable {
+        if (msg.value > state.price) {
+            contractOwner.transfer(msg.value);
+            state = Update(newURL, tx.origin, msg.value, tx.gasprice);
+            emit UpdateURL(state.url, state.price, state.owner);
+        } else {
+            revert();
+        }
+    }
+
+    function getURL() public view returns (string memory url) {
+        return state.url;
+    }
+
+    function getPrice() public view returns (uint256 price) {
+        return state.price;
+    }
+
+    function getOwner() public view returns (address owner) {
+        return state.owner;
+    }
+
+    function getGasPrice() public view returns (uint256 gasprice) {
+        return state.gasprice;
+    }
 }
